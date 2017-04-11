2d1cd0a6b910c3b47d2e472149307907f2092a96
Brayan27 <brgmarchena@hotmail.com>
2017-04-11 13:46:33 -0600

    Boton Edad Implementado

--- src/com/programa/sicovin/RegisterActivity.java
+++ src/com/programa/sicovin/RegisterActivity.java
@@ -4,13 +4,26 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
+
+	private CheckBox menorAnio;
+	private TextView textEdad;
+	private EditText reg_edad;
+	private EditText reg_edadMeses;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+
+		inicializarComponentes();
+		ajustarEventos();
 	}
 
 	@Override
@@ -30,6 +43,31 @@ public class RegisterActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	private void inicializarComponentes() {
+		menorAnio = (CheckBox) findViewById(R.id.checkBoxMenor);
+		textEdad = (TextView) findViewById(R.id.textEdad);
+		textEdad.setText("Edad en Años");
+		reg_edad = (EditText) findViewById(R.id.reg_edad);
+		reg_edadMeses = (EditText) findViewById(R.id.reg_edadMeses);
+	}
+
+	private void ajustarEventos() {
+		menorAnio.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				if (menorAnio.isChecked()) {
+					textEdad.setText("Edad en Meses");
+					reg_edad.setVisibility(View.GONE);
+					reg_edadMeses.setVisibility(View.VISIBLE);
+				} else {
+					textEdad.setText("Edad en Años");
+					reg_edadMeses.setVisibility(View.GONE);
+					reg_edad.setVisibility(View.VISIBLE);
+				}
+			}
+		});
 	}
 
 }
